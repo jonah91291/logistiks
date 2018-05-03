2cfbf0e3a0e9329afe3f475e3f9117a90a42f1d9
Jonah Ravi <jonah.ravi@techwave.net>
2018-05-03 15:02:36 +0530

    this is my 1st commit to the LOGISTIKS REPOSITORY DEVELOPMENT BRANCH

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,66 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { DealersComponent } from './dealers/dealers.component';
+import { MarketplaceComponent } from './marketplace/marketplace.component';
+import { CommunityComponent } from './community/community.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import {Routes, RouterModule} from '@angular/router';
+import { AppheaderComponent } from './appheader/appheader.component';
+import { AppfooterComponent } from './appfooter/appfooter.component';
 
 
+
+const routes: Routes =[
+  {
+    path:"",
+    redirectTo:'',
+    pathMatch:'full',
+  },
+  {
+    path:"Dealers",
+    component:DealersComponent
+  },  
+  {
+    path:"Marketplace",
+    component:MarketplaceComponent
+  },
+  {
+    path:"commuity",
+    component:CommunityComponent
+  },
+  {
+    path:"login",
+    component:LoginComponent
+  },
+  {
+    path:"signin",
+    component:SignupComponent
+  },
+  {
+    path:"Appfooter",
+    component:AppfooterComponent
+  },
+  {
+    path:"Appheader",
+    component:AppheaderComponent
+  }
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DealersComponent,
+    MarketplaceComponent,
+    CommunityComponent,
+    LoginComponent,
+    SignupComponent,
+    AppheaderComponent,
+    AppfooterComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
